edeec08279810b8d334a0b4eb734b7eed0a310c2
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-10-08 23:38:20 +0900

    レスポンシブ対応の記述

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import Event from './components/Event'
@@ -8,6 +7,16 @@ const Container = styled.div`
   background-color: blue;
   width: 100px;
   height: 100px;
+  @media screen and (min-width: 600px) {
+    background-color: green;
+    width: 150px;
+    height: 150px;
+  }
+  @media screen and (min-width: 1024px) {
+    background-color: red;
+    width: 200px;
+    height: 200px;
+  }
 `
 
 function App() {
