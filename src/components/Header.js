3a442b9a68cf3a324f30a746662550b4058bf345
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-10-09 03:40:52 +0900

    2カラム実装

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/react'
+
+const Container = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color: black;
+    position: fixed;
+    left: 0;
+    top: 0;
+    display: flex;
+    @media screen and (min-width: 1024px) {
+        width: 314px;
+        background-color: orange;
+        position: fixed;
+        left: 0;
+        top: 0;
+        display: block;
+    } 
+`
+
+
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+        </Container>
     )
 }
 
