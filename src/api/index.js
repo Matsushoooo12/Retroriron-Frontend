4ea2c5840fdb7b65b68aae5b8e950ee43e78e591
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-10-12 02:51:31 +0900

    Newsページ　API反映

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,7 @@ import client from "./client";
 export const getEvent = () => {
     return client.get('/events');
 }
+
+export const getNews = () => {
+    return client.get('/news')
+}
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,8 +1,28 @@
 import React, { useState } from 'react'
 import Helmet from 'react-helmet';
 import styled from '@emotion/styled';
-import Retroriron from '../images/retroriron.jpeg'
+import { getNews } from '../api';
+import { useEffect } from 'react/cjs/react.development';
 const News = () => {
+
+    // API
+
+    const [news, setNews] = useState([])
+    const handleGetNews = async () => {
+        try{
+            const res = await getNews();
+            setNews(res.data)
+            console.log(res.data)
+        } catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        handleGetNews();
+    }, [])
+
+    // アコーディオン
 
     const [content, setContent] = useState(null)
     const toggleAccordion = (i) => {
@@ -12,13 +32,6 @@ const News = () => {
         setContent(i);
     };
 
-    const data = [
-        {title: "1【2nd digital Single Life配信決定】", text: "1レトロリロン公開２曲目となる2nd digital SingleLifeが１０月中旬に配信決定!公開されると各種サブスクリプションにてご視聴いただけます。", date: "2021.9.21", image: `${Retroriron}`},
-        {title: "2【2nd digital Single Life配信決定】", text: "2レトロリロン公開２曲目となる2nd digital SingleLifeが１０月中旬に配信決定!公開されると各種サブスクリプションにてご視聴いただけます。", tag: "New", date: "2021.9.21"},
-        {title: "3【2nd digital Single Life配信決定】", text: "3レトロリロン公開２曲目となる2nd digital SingleLifeが１０月中旬に配信決定!公開されると各種サブスクリプションにてご視聴いただけます。", tag: "New", date: "2021.9.21"}
-    ]
-
-    console.log(content)
     return (
         <>
             <Helmet>
@@ -26,22 +39,20 @@ const News = () => {
                 <meta name="the News page of a pop band called Retroriron." content="news page" />
             </Helmet>
             <NewsContainer className="container">
-                {data.map((item, i) => (
+                {news.map((item, i) => (
                     <NewsItemContainer key={i}>
                         <NewsItemOtherContainer>
-                            <NewsTagContainer className={item.tag ? "" : "hidden"}>
-                                <NewsTagText>{item.tag}</NewsTagText>
-                            </NewsTagContainer>
+                            <NewsTagText>News</NewsTagText>
                             <NewsDate>{item.date}</NewsDate>
                         </NewsItemOtherContainer>
                         <NewsContentsContainer>
                             <NewsTitle className={content === i ? "active" : ""} onClick={() => toggleAccordion(i)}>{item.title}</NewsTitle>
                             <NewsTextContainer className={content === i ? "active" : ""}>
                                 <NewsText>
-                                    {item.text}
+                                    {item.content}
                                 </NewsText>
                                 {item.image ? (
-                                    <NewsImage src={item.image} />
+                                    <NewsImage className={item.image_vertical ? "vertical" : ""} src={process.env.REACT_APP_DEV_API_URL + item.image.url} />
                                 ):(
                                     <></>
                                 )}
@@ -67,7 +78,7 @@ const NewsContainer = styled.ul`
 // NewsItemContainer
 
 const NewsItemContainer = styled.li`
-    width: 60vw;
+    width: 64vw;
     border-top: 1px solid #BEBEBE;
     margin: 0 auto;
     padding: 24px 0;
@@ -83,29 +94,16 @@ const NewsItemOtherContainer = styled.div`
 
 // NewsTag
 
-const NewsTagContainer = styled.div`
-    width: 40px;
-    height: 24px;
-    border-radius: 3px;
-    background-color: #F42626;
-    position: relative;
-    margin-right: 16px;
-    &.hidden{
-        background-color: #fff;
-        z-index: 10;
-    }
-`
-
 const NewsTagText = styled.p`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    color: #fff;
+    color: #F42626;
     font-weight: 900;
     font-family: 'Noto Sans JP', sans-serif;
+    font-size: 1.2rem;
+    margin-right: 24px;
+    margin-top: 2px;
+    &.hidden{
+        color: #fff;
+    }
 `
 
 // NewsDate
@@ -117,6 +115,8 @@ const NewsDate = styled.p`
     color: #292929;
     margin-top: 3px;
 `
+
+// NewsContentsContainer
 
 const NewsContentsContainer = styled.div`
     display: block;
@@ -158,10 +158,16 @@ const NewsText = styled.p`
     margin-left: 16px;
     margin-top: 16px;
     margin-bottom: 16px;
+    white-space: pre-wrap;
 `
 
 const NewsImage = styled.img`
-    width: 300px;
     display: block;
     margin-left: 16px;
+    width: 300px;
+    height: 100%;
+    &.vertical{
+        height: 300px;
+        width: 200px;
+    }
 `
