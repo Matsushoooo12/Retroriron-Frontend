0b884fb03a6e35201b6705cf69da7b1b21326dc7
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-10-24 22:44:27 +0900

    全体のブロックの見た目修正

---
+++ src/App2.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import styled from '@emotion/styled';
+import Header from './components/common/Header2';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom'
+import Home from './pages/Home2';
+import Live from './pages/Live2';
+import News from './pages/News2';
+import Discography from './pages/Discography2';
+import Contact from './pages/Contact2';
+import './components/common/reset.css'
+import Footer from './components/common/Footer';
+
+function App() {
+  return (
+        <BrowserRouter>
+            <Header />
+            <MainAllContainer>
+                <MainItemContainer>
+                    <MainItem>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/news" component={News} />
+                            <Route exact path="/live" component={Live} />
+                            <Route exact path="/discography" component={Discography} />
+                            <Route exact path="/contact" component={Contact} />
+                        </Switch>
+                    </MainItem>
+                </MainItemContainer>
+                <Footer />
+            </MainAllContainer>
+        </BrowserRouter>
+  );
+}
+
+export default App;
+
+// const HeaderContainer = styled.div`
+//     position: fixed;
+//     left: 0;
+//     background-color: #000;
+//     width: 314px;
+//     height: 100vh;
+// `
+
+// const HeaderItems = styled.div`
+//     width: 100px;
+//     height: 100px;
+//     background-color: yellow;
+// `
+
+const MainAllContainer = styled.div`
+    font-family: 'Noto Sans JP', sans-serif;
+    color: #292929;
+    padding-top: 93px;
+    @media screen and (min-width: 768px){
+        padding-top: 100px;
+    }
+    @media screen and (min-width: 900px){
+        padding-top: 0;
+        position: absolute;
+        right: 0;
+        background-color: blue;
+        width: calc(100% - 250px);
+        overflow-y: scroll;
+    }
+    @media screen and (min-width: 1150px){
+        width: calc(100% - 314px);
+    }
+`
+
+const MainItemContainer = styled.div`
+    width: 80%;
+    height: 100%;
+    background-color: red;
+    margin: 0 auto;
+`
+
+const MainItem = styled.div`
+    @media screen and (min-width: 900px){
+        margin-top: 64px;
+    }
+`
