8980b424a0a7c5904ae0f0b57a40c51d93a0697c
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-11-01 02:24:25 +0900

    ページリロード→reset()

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -15,7 +15,7 @@ import NilImageUrl from '../images/nil_image.JPG'
 
 const Live = () => {
     // useForm
-    const { register, formState: { errors }, getValues, handleSubmit } = useForm();
+    const { register, formState: { errors }, getValues, handleSubmit, reset } = useForm();
 
     // isConfirmationVisibleにstateを持たせて、入力内容確認画面の表示・非表示をコントロール
     // isConfirmationVisibleの初期値はfalseで入力内容確認画面は非表示に
@@ -53,9 +53,8 @@ const Live = () => {
     }
 
     const handleClick = () => {
+        reset()
         setTicketValue({open: false})
-        // eslint-disable-next-line no-restricted-globals
-        location.reload()
     }
 
     // API
