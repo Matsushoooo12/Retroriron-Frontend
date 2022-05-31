420184763eb10e9fa16fd2c5d76344e070347703
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-05-31 10:53:59 +0900

    404　canonical削除

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -13,7 +13,6 @@ const Page404 = () => {
             音楽大学出身の個性豊かな4人組バンド、レトロリロンのWebサイトの404ページ。
           "
         />
-        <link rel="canonical" href="https://retroriron.com/news" />
       </Helmet>
       <p style={{ fontSize: '56px', fontWeight: 'bold' }}>404</p>
     </>
